750d8c2eef759ce3e14c049b34a5de19b027a87c
DEV <dev@step.rivne>
2023-06-27 20:50:12 +0300

    check forms

---
+++ 21_check_Form/js/main.js
@@ -0,0 +1,147 @@
+
+
+// let form = document.forms[0]
+
+// form.addEventListener('submit',(e)=>{
+//     // if(form.name.value.length == 0){
+//     //     form.name.style.borderColor = 'red'
+//     //     e.preventDefault()
+//     // }
+//     if(!checkPassOfDigit(form.password.value)){
+//         form.password.style.borderColor = 'red'
+//         e.preventDefault()
+//     }
+// })
+
+// function checkPassOfDigit(pass){
+//     for(let i = 0; i < 10; i ++)
+//     {
+//         if(pass.indexOf(i) != -1)
+//             return true
+//     }
+//     return false
+// }
+
+// function checkPassOfLetter(pass){
+//     for(let i = 0; i < 10; i ++)
+//     {
+//         if(pass.indexOf(i) != -1)
+//             return true
+//     }
+//     return false
+// }
+
+// form.submit()
+// let template = /1/
+
+// let line_1 = '123!-'
+// let line_2 = '234'
+// let line_3 = 'Lorem ipsum 21 doLor'
+
+// console.log('Example ',template)
+// console.log(line_1,' ---> ', template.test(line_1))
+// console.log(line_2,' ---> ', template.test(line_2))
+// console.log(line_3,' ---> ', template.test(line_3))
+
+// template = /12/
+// console.log('Example ',template)
+// console.log(line_1,' ---> ', template.test(line_1))
+// console.log(line_2,' ---> ', template.test(line_2))
+// console.log(line_3,' ---> ', template.test(line_3))
+
+// template = /[12]/
+// console.log('Example ',template)
+// console.log(line_1,' ---> ', template.test(line_1))
+// console.log(line_2,' ---> ', template.test(line_2))
+// console.log(line_3,' ---> ', template.test(line_3))
+
+// template = /\D/
+// console.log('Example ',template)
+// console.log(line_1,' ---> ', template.test(line_1))
+// console.log(line_2,' ---> ', template.test(line_2))
+// console.log(line_3,' ---> ', template.test(line_3))
+
+// template = /4$/
+// console.log('Example ',template)
+// console.log(line_1,' ---> ', template.test(line_1))
+// console.log(line_2,' ---> ', template.test(line_2))
+// console.log(line_3,' ---> ', template.test(line_3))
+
+// template = /[^234]/
+// console.log('Example ',template)
+// console.log(line_1,' ---> ', template.test(line_1))
+// console.log(line_2,' ---> ', template.test(line_2))
+// console.log(line_3,' ---> ', template.test(line_3))
+
+// template = /\W/
+// console.log('Example ',template)
+// console.log(line_1,' ---> ', template.test(line_1))
+// console.log(line_2,' ---> ', template.test(line_2))
+// console.log(line_3,' ---> ', template.test(line_3))
+
+
+// template = /^\D{2}/
+// console.log('Example ',template)
+// console.log(line_1,' ---> ', template.test(line_1))
+// console.log(line_2,' ---> ', template.test(line_2))
+// console.log(line_3,' ---> ', template.test(line_3))
+
+// template = /^\d+[!]/
+// console.log('Example ',template)
+// console.log(line_1,' ---> ', template.test(line_1))
+// console.log(line_2,' ---> ', template.test(line_2))
+// console.log(line_3,' ---> ', template.test(line_3))
+
+// let phone = '+380956523654'
+
+
+// template = /^\+[0-9]{12}$/
+// console.log(phone,template.test(phone))
+
+let str = 'HTTPS'
+template = /https?/i
+console.log(str, ' --> ', template.test(str))
+
+// template = /[0-9]{3}/g
+// let str_1 = '123abc456qwer114'
+// console.log(str_1,' ---> ', template.exec(str_1))
+// console.log(str_1,' ---> ', template.exec(str_1))
+// console.log(str_1,' ---> ', template.exec(str_1))
+
+
+// line_3 = '652 - 536 - 589'
+// console.log(line_3,' ---> ', line_3.replace(/[- ]/g,''))
+// console.log(line_3,' ---> ', line_3.replace(/[- ]/,''))
+// console.log(line_3,' ---> ', line_3.replace(/[- ]/,''))
+
+// console.log(line_3, '--->', line_3.split(/\D+/))
+// template = /\d+/g
+// console.log(line_3, ' ---> ', line_3.match(template))
+
+// let url = ' swapi.dev/api/planets/1/'
+// template = /^\s+\S+/
+// console.log(template.test(url))
+
+// let form = document.forms[0]
+// form.addEventListener('submit',(e)=>{
+//     if(!checkEmail(form.email)){
+//         form.email.style.border = "red"
+//         e.preventDefault()
+//     }
+// })
+// function checkEmail(email){
+//     return /@/.test(email)
+// }
+
+// form.name.addEventListener('input',()=>{
+//     text = form.name.value
+//     console.log(text)
+
+//     if(text[0] == " "){
+//         console.log(text[0],'run')
+//         form.name.style.borderColor = 'Red'
+//     }
+//     else{
+//         form.name.style.borderColor = 'black'
+//     }
+// })
