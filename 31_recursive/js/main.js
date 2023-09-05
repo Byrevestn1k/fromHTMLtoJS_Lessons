c781f0bb9421ff4b9018aed1f17c4bd93062f7a3
DEV <dev@step.rivne>
2023-09-05 21:12:17 +0300

    mines

--- 31_recursive/js/main.js
+++ 31_recursive/js/main.js
@@ -31,16 +31,56 @@ print(3,10)
 print(11,10)
 */
 
-function printAB(a,b){
-    if(a <= b){
-        console.log(a)
-        printAB(a+1,b)
-    }   
+// function printAB(a,b){
+//     if(a <= b){
+//         console.log(a)
+//         printAB(a+1,b)
+//     }   
+// }
+// function printBA(a,b){
+//     if(a <= b){
+//         console.log(b)
+//         printBA(a,b-1)
+//     }   
+// }
+// printBA(1,10)
+
+// 3. Напишіть функцію, яка виводить передане їй число паліндромом (число-перевертень).
+// Наприклад: число 1234 вивести як 4321
+ // 12345
+ // 54321
+//  let root = document.querySelector('.root')
+// function reverseNumber(number,root){
+//     if(number < 10){
+//         root.textContent += number
+//         return
+//     }
+//     root.textContent += number%10
+//     reverseNumber(parseInt(number/10),root)
+// }
+
+// reverseNumber(123456,root)
+
+// 12345
+// (5 * 10 + 4) --> (54 * 10 + 3) --> (543 * 10) + 2 --> 5432 * 10 + 1 --> 54321
+
+
+
+
+// function sumNumber(number){
+//     if(number < 10){
+//         return number
+//     }
+//     return number%10 + sumNumber(parseInt(number/10))
+// }
+// console.log(sumNumber(12345))
+
+function printSymbol(number)
+{
+    if(number == 0){return}
+    console.log('(')
+    printSymbol(number-1)
+    console.log(')')
 }
-function printBA(a,b){
-    if(a <= b){
-        console.log(b)
-        printBA(a,b-1)
-    }   
-}
-printBA(1,10)
+
+printSymbol(4)
